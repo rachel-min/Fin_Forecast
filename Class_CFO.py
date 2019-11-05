ad935463f95507fdb4ad5f0e9ff685a1472ee030
vzhu <vincent-jf.zhu@aig.com>
2019-11-05 17:29:18 +0800

    Time 0 Asset Information
    1) Time 0 asset holding
    2) Time 0 FI and Equity BSCR
    3) In [App_daily_portfolio_feed], refer to [Config_Rating_Mapping.py] and [BSCR_Asset_Risk_Charge_v1 under Config_BSCR.py]

--- Class_CFO.py
+++ Class_CFO.py
@@ -98,8 +98,8 @@ class cfo():
     def run_TP_forecast(self, input_irCurve_USD = 0, input_irCurve_GBP = 0):
         Corp_Proj.run_TP_forecast(self.fin_proj, self._proj_t, self._val_date, self._liab_val_base, self._liab_summary_base, self._input_liab_val_base['curve_type'], self._input_liab_val_base['numOfLoB'], self._input_liab_val_base['base_GBP'], base_irCurve_USD = input_irCurve_USD, base_irCurve_GBP = input_irCurve_GBP, cf_proj_end_date = self._input_liab_val_base['cf_proj_end_date'], cash_flow_freq = self._input_liab_val_base['cash_flow_freq'], recast_risk_margin = self._input_liab_val_base['recast_risk_margin'])
     
-    def run_fin_forecast(self):
-        Corp_Proj.run_fin_forecast(self.fin_proj, self._proj_t, self._input_liab_val_base['numOfLoB'], self._proj_cash_flows)        
+    def run_fin_forecast(self, Asset_holding, Asset_adjustment, ):
+        Corp_Proj.run_fin_forecast(self.fin_proj, self._proj_t, self._input_liab_val_base['numOfLoB'], self._proj_cash_flows, Asset_holding, Asset_adjustment)        
 
 
     '''
