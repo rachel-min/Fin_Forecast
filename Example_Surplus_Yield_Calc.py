SWP: corrected future market yield calculation based on the forward rate
the future investment yield should be based on the forward rate (not spot zero rate).

Example_Surplus_Yield_Calc.py
@@ -46,26 +46,28 @@ FI_surplus_model_port    = {'Port1' : {'Maturity' : 6, 'Rating' : 'A', 'Weight' 
 proj_year_frac = IAL.Date.yearFrac("ACT/365",  valDate, eval_date)
 weighted_yield = 0
 
+
 ###self.FI_surplus_model_port    = {'Port1' : {'Maturity' : '6Y', 'Rating' : 'A', 'Weight' : 0.5}, 'Port2': {'Maturity' : '6Y', 'Rating' : 'BBB', 'Weight' : 0.5}}
 for each_port, each_target in FI_surplus_model_port.items():
     each_maturity          = each_target['Maturity']
+    each_maturity_date     = IAL.Util.addTerms(eval_date, [str(each_maturity) + "Y"])[0]
     initial_spread_terms   = initial_spread[each_target['Rating']]
     ultimate_spread_terms  = ultimate_spread[each_target['Rating']]
-    ir_rate                = base_irCurve_USD.zeroRate(each_maturity)
+    fwd_rate               = base_irCurve_USD.fwdRate(eval_date, each_maturity_date)
     spread_term            = initial_spread['Term']
     
     each_initial_spread  = np.interp(each_maturity, spread_term, initial_spread_terms)
     each_ultimate_spread = np.interp(each_maturity, spread_term, ultimate_spread_terms)
     each_weighted_spread = np.interp(proj_year_frac, [0, ultimate_period], [each_initial_spread, each_ultimate_spread])
-    each_yield           = ir_rate + each_weighted_spread
+    each_yield           = fwd_rate + each_weighted_spread
     weighted_yield      += each_yield * each_target['Weight']
 
 
-test_yield = IAL_App.FI_Yield_Model_Port(valDate,                           \
+test_yield = IAL_App.FI_Yield_Model_Port(valDate,                          \
                                         eval_date,                         \
-                                        FI_surplus_model_port, \
-                                        initial_spread,        \
-                                        ultimate_spread,       \
-                                        ultimate_period,       \
+                                        FI_surplus_model_port,             \
+                                        initial_spread,                    \
+                                        ultimate_spread,                   \
+                                        ultimate_period,                   \
                                         curveType = curveType,             \
                                         base_irCurve_USD = base_irCurve_USD) 